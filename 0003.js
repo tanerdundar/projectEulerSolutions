ee3909452a17494d110a8c242ecadc410e2be16e
tanerdundar <t.dundar145@mail.com>
2023-02-25 21:41:16 +0300

    0003 solved

--- 0003.js
+++ 0003.js
@@ -1,6 +1,33 @@
 let number = 600851475143;
+let primeFactors=[];
+let biggestPrime;
+for (let i = 2; i < Math.floor(Math.sqrt(number)) + 1; i++){
+        if(number%i==0) {
+            if(isPrime(number/i)) {
+                primeFactors.push(number/i)
+            }
+            if(isPrime(i)) {
+                primeFactors.push(i)
+            }
 
-for (let i = 0; i < Math.floor(Math.sqrt(number)); i++) {
-
+        }
 }
-console.log(Math.floor(Math.sqrt(number)))
+let biggestNumber =0;
+for(let i =0;i<primeFactors.length;i++) {
+    if(primeFactors[i]>biggestNumber) {
+        biggestNumber=primeFactors[i]
+    }
+}
+console.log(biggestNumber)
+
+
+function isPrime(number) {
+    let isPrime =true;
+    for (let i = 2; i < Math.floor(Math.sqrt(number)) + 1; i++) {
+        if(number%i==0){
+            isPrime = false;
+            break;
+        }
+    }
+    return isPrime;
+}
