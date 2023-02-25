66b20946690f0818eafcd568ef7a84ed4054bdb2
tanerdundar <t.dundar145@mail.com>
2023-02-25 22:03:56 +0300

    0004 started

---
+++ 0004.js
@@ -0,0 +1,15 @@
+let palindromeProducts = [];
+
+for(let i=100;i<1000;i++) {
+    for(let j=100;j<1000;j++) {
+        let number =i*j
+        let numberToString=number.toString();
+        if(numberToString.length==5) {
+            
+
+        }
+
+        }
+
+}
+
