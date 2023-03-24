e683639657ea283c126191fa4ac8e9fc5022f361
tanerdundar <t.dundar145@mail.com>
2023-03-24 17:38:29 +0300

    updated 7

--- 0007.js
+++ 0007.js
@@ -15,20 +15,20 @@ result = primeNumbers[10000];
 
 console.log(result);
 
-function isPrime(num) {
-  let prime = true;
-  for (let i = 2; i < Math.sqrt(num) + 1; i++) {
-    if (num % i == 0) {
-      prime = false;
+// function isPrime(num) {
+//   let prime = true;
+//   for (let i = 2; i < Math.sqrt(num) + 1; i++) {
+//     if (num % i == 0) {
+//       prime = false;
 
-      break;
-    }
-  }
-  if (num < 2) {
-    prime = false;
-  }
-  if (num == 2) {
-    prime = true;
-  }
-  return prime;
-}
+//       break;
+//     }
+//   }
+//   if (num < 2) {
+//     prime = false;
+//   }
+//   if (num == 2) {
+//     prime = true;
+//   }
+//   return prime;
+// }
